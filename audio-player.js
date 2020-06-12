6f99b03f437331b6826aa498a9b35475402ba04d
Corey-Recai <coreycrecai@gmail.com>
2020-06-12 18:13:22 -0400

    Changes to home page layout

---
+++ audio-player.js
@@ -0,0 +1,40 @@
+function startAudio() {
+  if ($('#play-pause').html() == "play_arrow") {
+    $('#itc-radio').trigger("play");
+    $('#play-pause').html('pause');
+  } else {
+    if ($('#play-pause').html() == "pause") {
+      $('#itc-radio').trigger("pause");
+      $('#play-pause').html('play_arrow');
+    }
+  }
+
+  var aud = document.getElementById("itc-radio");
+  aud.ontimeupdate = function () {
+    myFunction()
+  };
+
+  function myFunction() {
+    document.getElementById("position").innerHTML = "00:" + ('0' + Math.floor(aud.currentTime)).slice(-2);
+    document.getElementById("seekbar").max = Math.floor(aud.duration);
+    document.getElementById("seekbar").value = Math.floor(aud.currentTime);
+    if (aud.ended) {
+      document.getElementById("seekbar").max = 0;
+      document.getElementById('play-pause').innerHTML = "play_arrow";
+      document.getElementById("position").innerHTML = "00:00";
+    }
+  }
+}
+
+function muteAudio() {
+  var bool = $("#itc-radio").prop("muted");
+  if ($('#muted').html() == "volume_up") {
+    $('#itc-radio').prop("muted", !bool);
+    $('#muted').html('volume_off');
+  } else {
+    if ($('#muted').html() == "volume_off") {
+      $('#itc-radio').prop("muted", !bool);
+      $('#muted').html('volume_up');
+    }
+  }
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+M.AutoInit();
+// Ajax load main content into page
+$(document).ready(function () {
+    $("#home").load("home.html");
+});
+// Load Weather Widget
+! function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}(document, 'script', 'weatherwidget-io-js');
